971b114a107191e78d5b1426a2c568b71687cd05
Kimhang-KDT <tkdehdjajsl@gmail.com>
2024-10-10 14:18:41 +0900

    테스트 목록 페이지 기본 ver

--- client/src/components/tests.tsx
+++ client/src/components/tests.tsx
@@ -1,12 +1,50 @@
-import React, { useState } from 'react';
-import { Box, Typography, Button, Paper, TextField, Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { getTestInfos } from '../services/api';
 
-interface Tests {
+interface TestsInfo {
   id: number;
   test_name: string;
   test_description: string;
 }
+
+const Tests: React.FC = () => {
+  const [testsInfo, setTestsInfo] = useState<TestsInfo[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchTestsInfo = async () => {
+      const data = await getTestInfos();
+      setTestsInfo(data);
+    };
+    fetchTestsInfo();
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>모의고사</TableCell>
+            <TableCell>과목</TableCell>
+            <TableCell>데이터 상태</TableCell>
+            <TableCell>풀이</TableCell>
+            <TableCell>모델</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {testsInfo.map((testInfo, index) => (
+            <TableRow key={index}>
+              <TableCell>{testInfo.test_name}</TableCell>
+              <TableCell>{testInfo.test_description}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default Tests;
 
